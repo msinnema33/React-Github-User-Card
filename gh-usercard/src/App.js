10c9504c3707aec9befe05617f1043f71a1b0bf7
Mike Sinnema <msinnema33@gmail.com>
2020-02-11 13:48:29 -0700

    axios call working

--- gh-usercard/src/App.js
+++ gh-usercard/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    users: [],
+
+  };
+  
+  componentDidMount(){
+    axios
+      .get('https://api.github.com/users/msinnema33')
+      .then(e => {
+        console.log('data: ', e);
+        this.setState({
+          users: e.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render(){
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
