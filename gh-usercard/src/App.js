4bfa1be7bca198289418eabe2d83440c2866f284
Mike Sinnema <msinnema33@gmail.com>
2020-02-11 14:42:38 -0700

    pulling account and followers need to style

--- gh-usercard/src/App.js
+++ gh-usercard/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    users: [],
+    user: [],
+    followers: []
 
   };
   
@@ -14,19 +15,41 @@ class App extends React.Component {
       .then(e => {
         console.log('data: ', e);
         this.setState({
-          users: e.data
+          user: e.data
         });
       })
       .catch(err => console.log(err));
   }
+
+  getFollowers = e => {
+    // e.preventDefault();
+    axios
+      .get('https://api.github.com/users/msinnema33/followers')
+      .then(res => {
+        console.log('follow', res);
+        this.setState({
+          followers: res.data,
+        });
+      })
+      .catch(err => console.log(err));
+  }
+ 
 
   render(){
 
   return (
     <div className="App">
       <header className="App-header">
-        
+        GitHub User 
+        Welcome: {this.state.user.name};
+        <img src={this.state.user.avatar_url} />
       </header>
+      <button onClick={this.getFollowers}>Get My Followers</button>
+        My Followers
+        <div className='followers'>
+          Name:
+        </div>
+
     </div>
   );
   }
